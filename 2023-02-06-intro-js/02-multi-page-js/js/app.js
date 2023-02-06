c705bfc5e96e12fc6a07ce22f4b33a479a230d45
Tony Grimes <acidtone@tonygrimes.com>
2023-02-06 09:24:29 -0700

    added 262-d01

---
+++ 2023-02-06-intro-js/02-multi-page-js/js/app.js
@@ -0,0 +1,12 @@
+let siteTitle = "WBDV - Winter 2023";
+
+
+
+// Adding content to the page
+// Ignore this code for now, we'll talk more about this in Days 6-10
+
+// Store <div> object as a variable
+const header = document.querySelector('.site-title div');
+
+// Update the content of the <div>
+header.innerHTML = siteTitle;
