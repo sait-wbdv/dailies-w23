267f4c351bbfd6c08b49b2a1859fc6491931ea66
Tony Grimes <acidtone@tonygrimes.com>
2023-02-07 07:48:53 -0700

    added 262-d1

--- 2023-02-06-intro-js/02-multi-page-js/js/app.js
+++ 2023-02-06-intro-js/02-multi-page-js/js/app.js
@@ -1,12 +1,14 @@
-let siteTitle = "WBDV - Winter 2023";
+let siteTitle = "Web Developer Fast Track";
 
 
-
-// Adding content to the page
-// Ignore this code for now, we'll talk more about this in Days 6-10
+/* Adding content to the page
+   Ignore this code for now, we'll talk more about this in Days 6-10 */
 
 // Store <div> object as a variable
 const header = document.querySelector('.site-title div');
 
 // Update the content of the <div>
 header.innerHTML = siteTitle;
+
+// Update the <title> of the page
+document.title = siteTitle;
