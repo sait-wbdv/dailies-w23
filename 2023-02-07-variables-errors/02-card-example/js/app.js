8c8981c866a01fbac6dc55a52aba595e24e68c10
Tony Grimes <acidtone@tonygrimes.com>
2023-02-07 10:47:21 -0700

    added sample card

---
+++ 2023-02-07-variables-errors/02-card-example/js/app.js
@@ -0,0 +1,23 @@
+// Add your code here
+
+/************************************/
+/* Do not edit code below this line */
+/************************************/
+
+const headingOutput = document.querySelector('h3');
+headingOutput.innerHTML = name;
+
+const picOutput = document.querySelector('img');
+picOutput.src = picURL;
+
+const nameOutput = document.querySelector('.name');
+nameOutput.innerHTML = name;
+
+const ageOutput = document.querySelector('.age');
+ageOutput.innerHTML = age;
+
+const hobbiesOutput = document.querySelector('.hobbies');
+hobbiesOutput.innerHTML = hobbies;
+
+const hungryOutput = document.querySelector('.hungry');
+hungryOutput.innerHTML = hungry;
