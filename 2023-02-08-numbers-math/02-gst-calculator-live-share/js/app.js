f8185c4452ff289654256e4147a358672ad03709
Tony Grimes <acidtone@tonygrimes.com>
2023-02-08 11:14:55 -0700

    added end-of-morning code

--- 2023-02-08-numbers-math/02-gst-calculator-live-share/js/app.js
+++ 2023-02-08-numbers-math/02-gst-calculator-live-share/js/app.js
@@ -1,8 +1,7 @@
-// Goal: 
-
-// 1. Define a sub total of a bill
-let billSubTotal; // The sub-total of a bill at the supermarket
-billSubTotal = prompt("How much is the bill?");
+ // 1. Define a sub total of a bill
+let billSubTotal = prompt("How much is the bill?"); // The sub-total of a bill at the supermarket
+// Convert to number
+billSubTotal = parseFloat(billSubTotal);
 
 // 2. Define a GST tax rate
 const gstTaxRate = 0.05;
@@ -11,7 +10,19 @@ const gstTaxRate = 0.05;
 const gstAmount = billSubTotal * gstTaxRate; // Additional tax amount on the bill sub-total: sub-total times the tax rate
 
 // 4. Define and calculate the bill total
-const billTotal =  parseFloat(billSubTotal) + gstAmount; // The total bill amount: sub-total plus the GST
+const billTotal = billSubTotal + gstAmount; // The total bill amount: sub-total plus the GST
 
+// 4a. Define the result as a complete sentence
+const result = `The GST on $${billSubTotal} is $${gstAmount} for a total of $${billTotal}.`;
 
-console.log(billSubTotal, gstTaxRate, gstAmount, billTotal);
+// 5. console.log() a complete sentence
+// console.log(billSubTotal, gstTaxRate, gstAmount, billTotal);
+console.log(result);
+
+// 6. Output the sentence to the page
+// 6a. Create a variable for <p> element
+const paragraph = document.querySelector('.bill-total');
+console.log(paragraph);
+
+// 6b. Set the content of the <p> to the sentence
+paragraph.innerHTML = result;
