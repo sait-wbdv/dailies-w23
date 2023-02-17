8cf0c3612fd1917e36a1ce47b47034247b6cd18a
Tony Grimes <acidtone@tonygrimes.com>
2023-02-17 10:49:13 -0700

    tweaks

--- 2023-02-17-conditional-code/03-calculator-if/js/app.js
+++ 2023-02-17-conditional-code/03-calculator-if/js/app.js
@@ -14,18 +14,23 @@ if (operation === 'add') {
 }
 
 if (operation === 'subtract') {
+  // Subtract the two numbers
   const difference = firstNum - secondNum;
   console.log(`The difference between ${firstNum} and ${secondNum} is ${difference}.`);
 }
 
 if (operation === 'multiply') {
+  // Multiply the two numbers
   const product = firstNum * secondNum;
   console.log(`The product of ${firstNum} and ${secondNum} is ${product}.`);
 }
 
 if (operation === 'divide') {
+  // Divide the two numbers
   const quotient = firstNum / secondNum;
   console.log(`The quotient of ${firstNum} and ${secondNum} is ${quotient}.`);
 }
 
+// Bug: This console log runs even if `operation` is a valid value
+// See 03a-calculator-elseif-else
 console.log('Please enter a valid operation');
