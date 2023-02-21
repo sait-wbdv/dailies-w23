8b4b365a9f737c63ff36332ea33e109cc4d782fa
Tony Grimes <acidtone@tonygrimes.com>
2023-02-21 09:00:23 -0700

    updated spoilers

--- 2023-02-21-logical-operators/01-achievement-6-spoilers/js/app.js
+++ 2023-02-21-logical-operators/01-achievement-6-spoilers/js/app.js
@@ -1,25 +1,33 @@
 // Don't forget prompt() always returns a string, even for numbers
-const number = parseFloat(prompt("Enter a number: "));
+let number = prompt("Enter a number: ");
+number = Number(number);
 
-// Step 1: Move these three console logs into the correct conditional blocks below
+// const number = 'twenty';
+
+// Step 1: Rearrange the console logs so that the statements are correct
 
 if (number > 100) {
   
   console.log("The number is greater than 100");
-
+  
 } else if (number > 0 && number < 101) {
-
+  
   console.log("The number is between 1 and 100");
-
+  
 } else if (number === 0) {
-
+  
   console.log("The number is 0");
   
-} else {
+} else if (number < 0) {
   
   console.log("The number is negative");
 
+} else {
+
+  console.log("This is not a number.");
+
 }
 
-// Step 2: Test and refactor your code to ensure these conditional statements are operating correctly (don't forget to test zero!). Hint: Remember that numbers can also be strings.
-
+// Step 2: Test and refactor your code to ensure these conditional statements are operating correctly
+// Don't forget to test zero!
+// Hint: Remember that numbers can also be strings.
