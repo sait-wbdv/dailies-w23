88c8ff53a27409da46ff4a099281b6b9c3d5f7d0
Tony Grimes <acidtone@tonygrimes.com>
2023-02-22 06:07:14 -0700

    added ach-7 starter

---
+++ 2023-02-22-functions/01-achievement-7-spoilers/js/app.js
@@ -0,0 +1,33 @@
+// Example 1: AND (&&) Operator
+// Using a logical AND operator (&&), refactor the following code so that ONE if statement is used to correctly nap when you are sleepy AND it's the afternoon.
+
+const timeOfDay = 'afternoon';
+const sleepy = true;
+
+if (timeOfDay === 'afternoon') {
+  if (sleepy) {
+    console.log('Time to have my afternoon nap!');
+  }
+}
+
+// Example 2: OR (||) Operator
+// Using a logical OR operator (||), refactor the following code into ONE if statement that contains ONE console log.
+
+const mood = 'happy';
+
+if (mood === 'stressed') {
+  console.log('Pass the potato chips!');
+}
+
+if (mood === 'hungry') {
+  console.log('Pass the potato chips!');
+}
+
+// Example 3: Logical NOT (!) Operator
+// Using a logical NOT operator (!), refactor the condition of the following if statement so that the statement is logged to the console correctly.
+
+const cloudy = true;
+
+if (cloudy) {
+  console.log('Put on sun glasses.');
+}
