954a3ece17ceac7c3ea2a045a289e8359a75dc8e
Tony Grimes <acidtone@tonygrimes.com>
2023-03-01 10:51:20 -0700

    added greet finished - pure function

--- 2023-03-01-pure-functions/03-greet-function-pure-function/js/greet.js
+++ 2023-03-01-pure-functions/03-greet-function-pure-function/js/greet.js
@@ -1,25 +1,34 @@
 // Step 1: Declare function
 const greet = function(name, lang) {
+  let greeting;
 
   if (lang === 'es') {
-    console.log(`Hola, ${name}!`);
-
+    // console.log(`Hola, ${name}!`);
+    greeting = `Hola, ${name}!`;
   } else if (lang === 'fr') {
-    console.log(`Bonjour, ${name}!`);
-
+    // console.log(`Bonjour, ${name}!`);
+    greeting = `Bonjour, ${name}!`;
   } else if (lang === 'ch') {
-    console.log(`Nihao, ${name}!`);
-
+    // console.log(`Nihao, ${name}!`);
+    greeting = `Nihao, ${name}!`;
   } else {
-    console.log(`Hi, ${name}!`);
+    // console.log(`Hi, ${name}!`);
+    greeting = `Hi, ${name}!`;
   }
+
+  return greeting;
 }
 
 // Step 2: Invoke function
-greet('Tony', 'ch');
+const greeting1 = greet('Tony', 'ch');
+console.log(greeting1);
 
-greet('Cordelle', 'fr');
+const greeting2 = greet('Cordelle', 'fr');
+console.log(greeting2);
 
-greet('Deadpool', 'es');
+const greeting3 = greet('Deadpool', 'es');
+console.log(greeting3);
 
-greet('The Last of Us', 'en');
+const greeting4 = greet('The Last of Us', 'en');
+console.log(greeting4);
+
