0991a3392c30685fd3e8a5b3726a68000e0680f0
Tony Grimes <acidtone@tonygrimes.com>
2023-03-01 10:56:37 -0700

    added comments

--- 2023-03-01-pure-functions/04-gst-pure-function/js/gst-calculator.js
+++ 2023-03-01-pure-functions/04-gst-pure-function/js/gst-calculator.js
@@ -1,15 +1,14 @@
 const gstRate = 0.05
 
 const calculateGST = function(subTotal) {
-  // Move repeated code here
+
+  // Use of `gstRate` makes this an impure function because
+  // it's not passed as an argument
   const gst = subTotal * gstRate;
   const total = subTotal + gst;
 
   console.log(`$${subTotal} plus ${gstRate * 100}% GST is $${total.toFixed(2)}.`);
 }
-
-// Refactor the following code so that gst totals are calculated by invoking calculateGST(). Example:
-// calculateGST(subTotal1);
 
 // Sub Total 1
 const subTotal1 = 40;
