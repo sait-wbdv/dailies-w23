8de5e401b7ced00ef171dcaee7ec9148ce2da154
Tony Grimes <acidtone@tonygrimes.com>
2023-03-10 10:07:48 -0700

    added for of code

--- 2023-03-10-for...of/03a-for...of-finised/js/app.js
+++ 2023-03-10-for...of/03a-for...of-finised/js/app.js
@@ -4,11 +4,15 @@ const treasureChest = ['Rusty knife', 'Gem', 'Kitten', 'Boots of Flight', 'Skele
 // Step 1: Create an empty output variable which will contain a string of the completed <li> elements;
 let output = '';
 
-
 // Step 2: Loop through the array using for...of;
+for (let item of treasureChest) {
+  // Step 3: At each iteration, add (i.e. concatenate) each array item (surrounded by <li> and </li> tags) to output;
 
+  console.log(item);
+  output += `<li>${item}</li>`;
 
-// Step 3: At each iteration, add (i.e. concatenate) each array item (surrounded by <li> and </li> tags) to output;
+  console.log(output);
+}
 
 
 // Step 4: After the loop completes, print the completed output variable to a <ul> on the page using .innerHTML.
