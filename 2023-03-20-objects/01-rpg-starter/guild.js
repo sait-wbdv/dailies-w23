3a1bc583b62cea22657937c86ecd5c34f2259a5c
Tony Grimes <acidtone@tonygrimes.com>
2023-03-20 06:55:42 -0600

    added d18 starter code

---
+++ 2023-03-20-objects/01-rpg-starter/guild.js
@@ -0,0 +1,83 @@
+/************/
+/* Player 1 */
+/************/
+
+// Player 1 general
+const player1Name = 'Ryan Reynolds';
+const player1Race = 'human';
+const player1Class = 'rogue';
+let player1Level = 1;
+
+// Player 1 stats
+let player1Strength = 7;
+let player1Dexterity = 12;
+let player1Constitution = 8;
+let player1Intelligence = 9;
+let player1Wisdom = 9;
+let player1Charisma = 11;
+
+// Player 1 effects
+let Player1Poisoned = true;
+
+// Player 1 items
+let player1Backpack = [
+    'rusty knife',
+    'gem',
+    'journal'
+  ];
+
+/************/
+/* Player 2 */
+/************/
+
+// Player 2 general
+const player2Name = 'Gyda';
+const player2Race = 'dwarf';
+const player2Class = 'fighter';
+let player2Level = 1;
+
+// Player 2 stats
+let player2Strength = 11;
+let player2Dexterity = 7;
+let player2Constitution = 11;
+let player2Intelligence = 7;
+let player2Wisdom = 8;
+let player2Charisma = 7;
+
+// Player 2 effects
+let player2Poisoned = false;
+
+// Player 2 items
+let player2Backpack = [
+  'sword',
+  'leather armour',
+  'rations'
+];
+
+/************/
+/* Player 3 */
+/************/
+
+// Player 3 general
+const player3Name = 'Anna';
+const player3Race = 'elf';
+const player3Class = 'wizard';
+let player3Level = 1;
+
+// Player 3 stats
+let player3Strength = 8;
+let player3Dexterity = 9;
+let player3Constitution = 8;
+let player3Intelligence = 11;
+let player3Wisdom = 10;
+let player3Charisma = 8;
+
+// Player 3 effects
+let player3Poisoned = true;
+
+// Player 3 items
+let player3Backpack = [
+  'staff',
+  'cloak',
+  'spell book'
+];
